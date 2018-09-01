89163dfb9a9643fba961c5b3a8f79310acb398c0
kdy0573 <kdy0573@daum.net>
2018-09-01 14:58:27 +0900

    데이터 베이스 연결 및 코드 정리 (1)

--- src/component/PerformList/PerformList.js
+++ src/component/PerformList/PerformList.js
@@ -5,7 +5,6 @@ import PerformanceCard from '../../component/PerformanceCard/PerformanceCard';
 const PerformList = ({data})=>{
 
      console.log("performListData",data);
-    //  console.log("performList",data.length); 
 
     let listOfDate = data.map((info,index)=>{
         console.log("PerformList js info : ",info);
@@ -21,30 +20,6 @@ const PerformList = ({data})=>{
     )
 }
 
-// class PerformList extends Component {
-//     constructor(props){
-//         super(props);
-//         console.log(props);
-//         this.state={
-//             data:props
-//         }
-//     }
-
-//     render(){
-//         console.log(this.state.data);
-//         console.log(this.state.data[0]);
-//         console.log(this.state.data[1]);
-//             let listOfDate = this.state.data.map((info,index)=>{
-//                 console.log("PerformList js info : ",info);
-//                 return <PerformanceCard key={index} date = {info.date} performInfo = {info.performInfo}></PerformanceCard>
-//             })
-//         return(
-//             <div>
-//                {listOfDate}
-//             </div>
-//         )
-//     }
-// }
 export default PerformList;
 
 PerformList.propTypes = {
--- src/container/infoContainer/InfoContainer.js
+++ src/container/infoContainer/InfoContainer.js
@@ -1,32 +1,25 @@
 import React, { Component } from 'react';
 import InfoWrapper from '../../component/InfoWrapper/InfoWrapper';
 import PerformList from '../../component/PerformList/PerformList';
-import TableComponent from '../../component/TableComponent/TableComponent';
-import data from '../../data/data.json';
-import axios from 'axios';
+import * as service from '../../service/service';
+// import axios from 'axios';
 class InfoContainer extends Component {
     
     constructor(props){
         super(props);
         this.state = {
             storeInfo : "전체",
-            // storeInfo : "jazz Alley",
             origin_performData:"",
             storeList:[],
             filter_performData:"",
         }
-        // this.handleChange = this.handleChange.bind(this);
+        
     }   
-
 
     //selectBox 가게 정보 바뀔때 
     handleChange=(e)=>{
-        // alert("change Store");
-        // console.log(e.target.value);
-        debugger;
         let filterID = e.target.value;
         let performData = this.state.origin_performData;
-        // console.log("filterData : ",performData)
     
         if(filterID==="전체"){
             this.setState({
@@ -42,79 +35,32 @@ class InfoContainer extends Component {
                      date: d.date,
                      performInfo:  performFilter
                  }
-                //  console.log("pre: ", data);
                  return data;
              })
              console.log("filter한 데이터 : ", performFilter)
              this.setState({
                  storeInfo : e.target.value,
                  filter_performData : performFilter
-             })
-             console.log("데이터 변경 : ",this.state)
+             })             
         }
-
-      
-
     }
-    // componentDidMount(){
 
-    // }
-
-    //rendering하기 전에 state에 데이터 삽입
-    loadData=()=>{
-    // componentWillMount(){
+    //데이터 받아오기전의 초기 상태 (default )
+    initData=()=>{
         console.log("loadData");
             const performData = [
                 {
-                date: new Date('2018-08-10').toLocaleDateString('se'),
+                date: new Date('1000-11-11').toLocaleDateString('se'),
                 performInfo : [
                     {
-                        PerformanceName : 'Sloppy notes',
-                        location : 'jazz Alley'
-                    },
-                    {
-                        PerformanceName : 'Brass Monkeys ',
-                        location : 'all that jazz'
-                    },
-                    {
-                        PerformanceName : 'Wave',
-                        location : 'all that jazz'
-                    },
-                    {
-                        PerformanceName : 'Sloppy notes',
-                        location : 'jazz Alley'
+                        PerformanceName : '데이터 로딩중',
+                        location : '잠시만 기다려주세요'
                     }
                 ] 
-                },
-                {
-                    date: new Date('2018-08-11').toLocaleDateString('se'),
-                    performInfo : [
-                        {
-                        PerformanceName : 'Sloppy notes',
-                        location : 'jazz Alley'
-                        },
-                        {
-                        PerformanceName : 'Brass Monkeys ',
-                        location : 'all that jazz'
-                        },
-                        {
-                        PerformanceName : 'Wave',
-                        location : 'all that jazz'
-                        },
-                        {
-                        PerformanceName : 'Sloppy notes',
-                        location : 'jazz Alley'
-                        }
-                    ] 
                 }
             ]
             //데이터베이스에서 가게에 대한 정보만 가지고오는방법 vs 일자별 정보를 가지고와서 javascript에서 필터할때
-            const storeList = ["전체","jazz Alley","all that jazz"] ;
-
-            // console.log(storeList);
-            // console.log(performData);
-            // console.log(storeInfo);
-
+            const storeList = ["로딩중"] ;
             this.setState({
                 storeInfo: this.state.storeInfo,
                 origin_performData: performData,
@@ -123,78 +69,66 @@ class InfoContainer extends Component {
             });
             
     }
-
-    loadDataFile = ()=>{
-        console.log("loadData");
-        //가게 이름 정보 
-        const storeList = ["전체","jazz Alley","all that jazz"] ;
-        console.log("1",data.result);
-        var orinData=null;
-        axios.get("http://localhost:3001/jazz")
-        .then(res=>{
-            console.log(res.data);
-            orinData = res.data;
-        })
-        .catch(error=>console.log(error));
-        this.setState({
-            storeInfo: this.state.storeInfo,
-            origin_performData: orinData,
-            storeList :storeList,
-            filter_performData:[]
-        });
-    }
+    
     //backend에서 불러오기
-    loadDatafromBack=()=>{
+    loadDatafromBack= async ()=>{
+
         //await 로 불러오기 
-        // fetch("http://localhost:3001/jazz")
-        // .then((res)=>console.log(res.json()))
-        // .catch(err=>console.log(err));
-        const url = "http://localhost:3001/jazz";
-        return axios.get(url)
+        const storeList = ["전체"] ;
+
+         await service.loadStoreInfo()
+         .then(response=>{
+            console.log(response.data);
+            console.log("resresrses");
+            this.setState({
+                        storeInfo: this.state.storeInfo,
+                        origin_performData: response.data.result,
+                        storeList :storeList,
+                        filter_performData:[]
+            });
+         });
+
+        await service.loadStoreName()
+        .then(response=>{
+            // response.data.result;
+            response.data.result.splice(0, 0, "전체");
+            this.setState({
+                ...this.state,storeList:response.data.result
+            })
+        })
+        
+
     }
     componentWillMount(){
-        this.loadData();
-        // this.loadDataFile();
-        //await
-        // const data = this.loadDatafromBack();
-        // console.log("init Data : ",data)
+        this.initData();//default 데이터 로징 전에 출력 하는 곳 
+        this.loadDatafromBack();//데이터베이스를 설치 안할시에는 우선 이부분 주석처리하고 하기 
+        
     }
     render() {
  
-        // this.loadData();
-        console.log("render : state : ",this.state);
-        // this.loadDataFile();
-        // console.log("origin : ",this.state.origin_performData);
-        // console.log("filter",this.state.filter_performData);
-
         const selectBox = this.state.storeList.map((d,index)=>{
             return <option key={index} value={d}>{d}</option>
         })
         //filter로 할지 아니면 전체로 할지 정하기 
         const filter_Data = ()=>{
             if(this.state.storeInfo==="전체"){
-                console.log("after Filter",this.state.filter_performData);
+                // console.log("after Filter",this.state.filter_performData);
                 return <PerformList data = {this.state.origin_performData}></PerformList>;
             }else{
-                console.log("after Filter",this.state.filter_performData);
+                // console.log("after Filter",this.state.filter_performData);
                 return <PerformList data = {this.state.filter_performData}></PerformList>;
             }
         }
         
-        // console.log("truefalse : ",(this.state.storeInfo==="전체"),this.state.filter_performData)
-        // console.log("필터 선택 데이터 : ",filterData)
         return (   
             <InfoWrapper>
                 <div>
                     2018년 8월 11일 공연이 있습니다.  
                     <label> 가게 정보</label>
-                    {/* <select value={this.state.storeInfo} onChange={changeStoreInfo()}> */}
                     <select value={this.state.storeInfo} onChange={this.handleChange}>
                         {selectBox}
                     </select>
                 </div>
-                {/* <TableComponent data = {performInfo}/> */}
-                {/* <PerformList data = {this.state.origin_performData}></PerformList> */}
                 {filter_Data()}
             </InfoWrapper>
         );
